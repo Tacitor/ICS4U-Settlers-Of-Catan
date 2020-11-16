28b401ff5e8d75f246ab9ee9f093110f974b6656
Tacitor <lukasjkrampitz@gmail.com>
2020-11-15 23:42:14 -0500

    change the tiles to use the new scaling

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GameFrame.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GameFrame.java
@@ -26,7 +26,7 @@ public class GameFrame extends javax.swing.JFrame {
     private void initFrame() {
         setTitle("Settlers of Catan");
         setSize(1920, 1080); //set the size to 1080p
-        setSize(1280, 720);
+        //setSize(1280, 720);
         setDefaultCloseOperation(EXIT_ON_CLOSE); //make sure it closes the thread when the frame closes
         add(new GamePanel(this));
         setLocationRelativeTo(null); //center the frame on screen
