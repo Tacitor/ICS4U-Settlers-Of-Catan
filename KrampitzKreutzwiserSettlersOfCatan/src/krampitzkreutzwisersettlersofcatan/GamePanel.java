9fb39d6663a991279a79728635764be5b9291fb0
Evan-Kreutzwiser <Evan@Evans-Desktop>
2020-11-17 11:13:50 -0500

    Build button selection is only changed if the selected button is disabled

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GamePanel.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GamePanel.java
@@ -20,6 +20,7 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.ButtonModel;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -853,7 +854,8 @@ public class GamePanel extends javax.swing.JPanel {
         boolean canBuildRoad; // If the user has enough cards to build these
         boolean canBuildSettlement;
         boolean canBuildCity;
-        
+        ButtonModel oldSelection; // The button selected before this update began
+
         // If the game is in setup
         if (inSetup) {
             canBuildRoad = (playerSetupRoadsLeft > 0);
@@ -866,7 +868,10 @@ public class GamePanel extends javax.swing.JPanel {
             canBuildSettlement = hasCards(1); // Settlements
             canBuildCity =       hasCards(2); // Cities
         }
-        
+
+        // Save what button was selected before this update began
+        oldSelection = buildBtnGroup.getSelection();
+
         // Select the first enabled button on the list
         if (canBuildRoad) {
             // Select the road button
@@ -899,6 +904,11 @@ public class GamePanel extends javax.swing.JPanel {
         buildSettlementSRBtn.setEnabled(canBuildSettlement); // Settlements
         buildSettlementLRBtn.setEnabled(canBuildCity);       // Cities
 
+        // If the button selected before this update is still enabled, select it
+        // instead of the selection made in the if/else block above
+        if (oldSelection != null && oldSelection.isEnabled()) { // Also make sure the saved button is not null
+            buildBtnGroup.setSelected(oldSelection, true); 
+        }
         // If any of the buttons are enabled, enable the build button
         // Otherwise disable it
         buildBtn.setEnabled(canBuildRoad || canBuildSettlement || canBuildCity);
