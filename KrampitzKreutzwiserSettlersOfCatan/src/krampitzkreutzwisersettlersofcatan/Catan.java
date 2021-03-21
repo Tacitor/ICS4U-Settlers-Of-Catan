de9f3b32414e8854bec748bfaa5899bd111e2870
Tacitor <lukasjkrampitz@gmail.com>
2021-03-20 23:27:36 -0400

    add a new class and move the main method to it. This new class contains a loop and a clock that can be used for animation within the game.

---
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/Catan.java
@@ -0,0 +1,56 @@
+/*
+ * Lukas Krampitz
+ * Mar 20, 2021
+ * Hold the main method and the clock for animation frames
+ */
+package krampitzkreutzwisersettlersofcatan;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class Catan {
+    
+    public static int clock = 0;
+    
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hi");
+        
+        /* Set the Windows 10 look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainMenu().setVisible(true);
+            }
+        });
+        
+        while (true) {
+            clock++;
+            Thread.sleep(1000); //time a spent sleeping is subject to change
+            //the way sleep is envoked is also subject to change.
+        }
+    }
+    
+}
