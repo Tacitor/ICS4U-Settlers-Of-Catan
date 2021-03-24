0e64267bd3118320d14ed1d92c81abf6024bdc35
Tacitor <lukasjkrampitz@gmail.com>
2021-03-23 20:56:20 -0400

    begin fixing player has port detection. Started work on a list of valid settlements for each port.

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GamePanel.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GamePanel.java
@@ -96,6 +96,7 @@ public class GamePanel extends javax.swing.JPanel {
     // ^^^ valid number are: 1 (knight), 2 (progress card road building), 3 (progress card monolpoy), 4 (progress card year of pleanty), 5, 6, 7, 8, 9 (5-9 are vp cards)
     private final ArrayList<Integer> availableDevCards; //a list of dev cards that are still in a pile and have not been drawn. 
     // ^^^ This ensures that the type distrobution is correct and also ensures that there will be a finite number of dev cards
+    private ArrayList<NodeSettlement>[] portSettlements; //Contains settlments that are on ports. One arrayList for each of the 9 ports.
     private final int victoryPoints[];
     private final int totalCardsCollected[];
     private final int[] cardStackXPositions; //the x positions to draw cards when in stacked mode
@@ -339,6 +340,16 @@ public class GamePanel extends javax.swing.JPanel {
         loadTiles(); //load the ArrayList of tiles with position and type data
         loadNodes(); // Create and link all of the board's settlement and road nodes
         loadPorts(); //read in all the data about the ports and populate the Array List with Ports
+
+        //port supporting vars
+        portSettlements = new ArrayList[ports.size()]; //create the same amount of ArrayLists as there are ports
+
+        //fill the array of ports settlements with new arrayLists
+        for (int i = 0; i < portSettlements.length; i++) {
+            portSettlements[i] = new ArrayList<>();
+        }
+
+        generatePortSettlements(); //Get a list of the settlements that allow access to each port
 
         // Add the mouse listener that calls the mouse click event handler
         addMouseListener(new MouseAdapter() {
@@ -1401,7 +1412,7 @@ public class GamePanel extends javax.swing.JPanel {
                                     // Set the settlement's player to the current player
                                     settlementNodes.get(i).setPlayer(currentPlayer);
 
-                                    Tile portLinkedTile; //the tile linked to the port to check agaist
+                                    Tile portLinkedTile; //the tile linked to the port to check against
                                     boolean onCoast; //whether or not the node is on the coast (on the null tile)
                                     boolean onPortTile; //whether or not the node is on a tile that is the linkedTile for a port
 
@@ -2484,7 +2495,7 @@ public class GamePanel extends javax.swing.JPanel {
             for (int i = 0; i < playerTurnOrder.size(); i++) {
                 saveFile.println(playerTurnOrder.get(i));
             }
-            
+
             //save the stealCardNum
             saveFile.println("stealCardNum:");
             saveFile.println("size:");
@@ -3231,7 +3242,7 @@ public class GamePanel extends javax.swing.JPanel {
             } else {
                 thrownLoadError = throwLoadError(thrownLoadError);
             }
-            
+
             //load in the stealCardNum
             if (scanner.nextLine().equals("stealCardNum:")) {
 
@@ -3270,7 +3281,7 @@ public class GamePanel extends javax.swing.JPanel {
             instructionLbl.setText("Use your cards to build roads or settlements");
             subInstructionLbl.setText("Or end your turn to continue the game");
         }
-        
+
         //if doing snake rules update the sub plays to match what it's supposed to be
         if (doSnakeRules && inSetup) {
             //update the playerTurnOrder
@@ -3283,7 +3294,7 @@ public class GamePanel extends javax.swing.JPanel {
         //get the number of each card type the player has
         countCardTypes(cards[currentPlayer].size());
         countNumCardTypes();
-        
+
         //update the text of the Swing buttons
         //check if building
         if (buildingObject != 0) {
@@ -3308,7 +3319,7 @@ public class GamePanel extends javax.swing.JPanel {
         }
 
         updateBuildButtons();
-        repaint();        
+        repaint();
 
         return !thrownLoadError; //return the success of loading. If no error was thrown then the load was a success.
     }
@@ -4731,9 +4742,24 @@ public class GamePanel extends javax.swing.JPanel {
                     //draw the boarder
                     g2d.setColor(new java.awt.Color(255, 255, 225));
                     g2d.drawRect(settlement.getXPos() - getImgWidth(image) / 2, settlement.getYPos() - getImgHeight(image) / 2, getImgWidth(image), getImgHeight(image));
-                    g2d.setColor(Color.black);
                 }
             }
+        }
+
+        //temp draw settlemt ports
+        for (int i = 0; i < portSettlements.length; i++) {
+
+            for (NodeSettlement node : portSettlements[i]) {
+
+                g2d.drawRect(node.getXPos() - getImgWidth(RED_HOUSE_S) / 2,
+                        node.getYPos() - getImgHeight(RED_HOUSE_S) / 2,
+                        getImgWidth(RED_HOUSE_S),
+                        getImgHeight(RED_HOUSE_S));
+
+                g2d.drawString(Integer.toString(i), node.getXPos(), node.getYPos());
+
+            }
+
         }
 
         // If a turn is currently going on, render the current player's cards
@@ -5851,6 +5877,73 @@ public class GamePanel extends javax.swing.JPanel {
         for (int i = 0; i < cards[currentPlayer].size(); i++) {
             numCardType[cards[currentPlayer].get(i)]++;
         }
+    }
+
+    /**
+     * Get a list of the settlements that allow access to each port.
+     */
+    private void generatePortSettlements() {
+
+        ArrayList<Tile> portTiles = new ArrayList<>();
+        boolean onCoast;
+        boolean onPortTile;
+        int portNum;
+        int altPortNum; //if there is another portnum
+
+        //loop through the ports and get a list of their Tiles
+        for (Port port : ports) {
+            portTiles.add(port.getLinkedTile());
+        }
+
+        //loop through the settlements
+        for (NodeSettlement node : settlementNodes) {
+
+            //reset the bool vars
+            onCoast = false;
+            onPortTile = false;
+
+            //reset the ints
+            portNum = 0;
+            altPortNum = 0;
+
+            //loop through the 3 tile the settlement is on use int range 1-3
+            //go through backwards
+            for (int k = 1; k < 4; k++) {
+                //check if this tile is the same as a port's tile
+                if (portTiles.contains(node.getTile(k))) {
+                    onPortTile = true;
+
+                    //save the port num
+                    //only use the alt if the main is already used
+                    if (portNum == 0) {
+                        portNum = portTiles.indexOf(node.getTile(k));
+                    } else {
+                        altPortNum = portTiles.indexOf(node.getTile(k));
+                    }
+                }
+
+                //also check if this tile is the null tile meaning the settlemnt is on the coast
+                if (node.getTile(k) == null) {
+                    onCoast = true;
+                }
+            }
+
+            //only register the port as owned if the conditions are met
+            //also only add it if there are less than 3 settlements. Limitiing a port to having max 3
+            if (onCoast && onPortTile) {
+
+                //check how many settlemtnts that port already has
+                if (portSettlements[portNum].size() < 3) {
+
+                    //save that the new settlement is on a port and which one
+                    portSettlements[portNum].add(node);
+                } else if (portSettlements[altPortNum].size() < 3) {
+                    portSettlements[altPortNum].add(node);
+                }
+            }
+
+        }
+
     }
 
     /**
