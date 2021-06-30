879305dcfb487c4f28fb8879343fbe9f29d29d09
Tacitor <lukasjkrampitz@gmail.com>
2021-06-30 19:02:14 -0400

    change the way the exit buttons work with leaving online play on the game panel. Now they call a single method that is the same between them. This method stopes the CSC and also resets the static onlineMode and onlineClient vars to the offline defaults.

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GamePanel.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/GamePanel.java
@@ -729,10 +729,7 @@ public class GamePanel extends javax.swing.JPanel {
             //save the game and only close if it is successful
             if (save()) {
 
-                //if there is networking active stop it
-                if (onlineMode != -1) {
-                    onlineClient.sendStop(); //tell the server that this client disconected and to close the server
-                }
+                networkingCloseOpertations();
 
                 // Hide this window and show the main menu
                 superFrame.getMainMenu().setVisible(true); //show the main menu
@@ -1071,16 +1068,27 @@ public class GamePanel extends javax.swing.JPanel {
         //If the user really want to leave let them
         if (overwrite == 0) {
 
-            //if there is networking active stop it
-            if (onlineMode != -1) {
-                onlineClient.sendStop(); //tell the server that this client disconected and to close the server
-            }
-
+            networkingCloseOpertations();
+            
             // Hide this window and show the main menu
             superFrame.getMainMenu().setVisible(true); //show the main menu
             superFrame.setVisible(false); //hide the parent frame 
         }
     }//GEN-LAST:event_backNoSaveBtnActionPerformed
+
+    /**
+     * Tasks that need to be performed when closing out of game from online mode
+     */
+    private void networkingCloseOpertations() {
+        //if there is networking active stop it
+        if (onlineMode != -1) {
+            onlineClient.sendStop(); //tell the server that this client disconected and to close the server
+            
+            //reset the game to the offline defaults
+            onlineMode = -1;
+            onlineClient = null;
+        }
+    }
 
     private void trade3to1BtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_trade3to1BtnActionPerformed
         //check what mode of trading the game is in for 3:1
@@ -5529,12 +5537,11 @@ public class GamePanel extends javax.swing.JPanel {
             }
 
         }
-        
+
         /*
          * =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= End SetterBtn Drawing =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
          *
          */
-
         //draw the separator boxes around the UI elements
         //only if the user want to
         if (showMenuBoarder) {
