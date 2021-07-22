19ab7865e213990f126717e574e37c48b55ea84b
Tacitor <lukasjkrampitz@gmail.com>
2021-07-22 16:51:34 -0400

    format the settlement node class

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/worldObjects/NodeSettlement.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/worldObjects/NodeSettlement.java
@@ -6,7 +6,6 @@
  */
 package krampitzkreutzwisersettlersofcatan.worldObjects;
 
-
 public class NodeSettlement extends WorldObject {
 
     // Attributes
@@ -22,7 +21,6 @@ public class NodeSettlement extends WorldObject {
     private Tile hex2;
     private Tile hex3;
 
-    
     /**
      * Constructor to create a new blank settlement node
      */
@@ -30,7 +28,7 @@ public class NodeSettlement extends WorldObject {
         // Initialize the position
         xPos = 0;
         yPos = 0;
-        
+
         // Initialize settlement attributes
         player = 0;
         large = false;
@@ -38,9 +36,10 @@ public class NodeSettlement extends WorldObject {
         road2 = null;
         road3 = null;
     }
-    
+
     /**
      * Constructor to create a new settlement node at a specific position
+     *
      * @param xPos The new node's X position
      * @param yPos The new node's Y position
      * @param refNum
@@ -48,15 +47,16 @@ public class NodeSettlement extends WorldObject {
     public NodeSettlement(int xPos, int yPos, int refNum) {
         // Initialize the node
         this(); // With constructor chaining
-        
+
         // Set the position to the passed values
         this.xPos = xPos;
         this.yPos = yPos;
         this.refNum = refNum;
     }
-    
+
     /**
      * Constructor to create a new settlement node with road connections
+     *
      * @param xPos The new node's X position
      * @param yPos The new node's Y position
      * @param road1 One of the connected roads
@@ -67,15 +67,16 @@ public class NodeSettlement extends WorldObject {
     public NodeSettlement(int xPos, int yPos, NodeRoad road1, NodeRoad road2, NodeRoad road3, int refNum) {
         // Initialize the node
         this(xPos, yPos, refNum); // With constructor chaining
-        
+
         // Set the road references
         this.road1 = road1;
         this.road2 = road2;
         this.road3 = road3;
     }
-    
+
     /**
      * Constructor to create a new settlement node with world tile references
+     *
      * @param xPos The new node's X position
      * @param yPos The new node's Y position
      * @param hex1 One of the adjacent tiles
@@ -86,15 +87,16 @@ public class NodeSettlement extends WorldObject {
     public NodeSettlement(int xPos, int yPos, Tile hex1, Tile hex2, Tile hex3, int refNum) {
         // Initialize the node
         this(xPos, yPos, refNum); // With constructor chaining
-        
+
         // Set the tile references
         this.hex1 = hex1;
         this.hex2 = hex2;
         this.hex3 = hex3;
     }
-    
+
     /**
      * Constructor to create a new settlement node with world tile references
+     *
      * @param xPos The new node's X position
      * @param yPos The new node's Y position
      * @param road1 One of the connected roads
@@ -105,19 +107,20 @@ public class NodeSettlement extends WorldObject {
      * @param hex3 A third tiles
      * @param refNum
      */
-    public NodeSettlement(int xPos, int yPos, NodeRoad road1, NodeRoad road2, 
+    public NodeSettlement(int xPos, int yPos, NodeRoad road1, NodeRoad road2,
             NodeRoad road3, Tile hex1, Tile hex2, Tile hex3, int refNum) {
         // Initialize the node and set road references
         this(xPos, yPos, road1, road2, road3, refNum); // With constructor chaining
-        
+
         // Set the tile references
         this.hex1 = hex1;
         this.hex2 = hex2;
         this.hex3 = hex3;
     }
-    
+
     /**
      * Get the player ID of this settlement's owner
+     *
      * @return The player ID, or 0 if not built upon yet
      */
     public int getPlayer() {
@@ -125,8 +128,9 @@ public class NodeSettlement extends WorldObject {
     }
 
     /**
-     * Set the player ID of this settlement's owner
-     * This should only be called once, to avoid violating game rules
+     * Set the player ID of this settlement's owner This should only be called
+     * once, to avoid violating game rules
+     *
      * @param player The new player ID
      */
     public void setPlayer(int player) {
@@ -135,6 +139,7 @@ public class NodeSettlement extends WorldObject {
 
     /**
      * Get the size of this settlement
+     *
      * @return If the settlement is a large settlement
      */
     public boolean isLarge() {
@@ -142,32 +147,39 @@ public class NodeSettlement extends WorldObject {
     }
 
     /**
-     * Set the size of the settlement
-     * This should only be called once, to increase the size, to avoid violating game rules
-     * @param isLarge The new size, as a boolean representing if it is a large settlement
+     * Set the size of the settlement This should only be called once, to
+     * increase the size, to avoid violating game rules
+     *
+     * @param isLarge The new size, as a boolean representing if it is a large
+     * settlement
      */
     public void setLarge(boolean isLarge) {
         this.large = isLarge;
     }
-    
+
     /**
-     * Get the reference number of the settlement. This is the spot in the ArrayList it will occupy
-     * @return 
+     * Get the reference number of the settlement. This is the spot in the
+     * ArrayList it will occupy
+     *
+     * @return
      */
     public int getRefNum() {
         return refNum;
     }
-    
+
     /**
-     * Set the reference number of the settlement. This is the spot in the ArrayList it will occupy
-     * @param refNum 
+     * Set the reference number of the settlement. This is the spot in the
+     * ArrayList it will occupy
+     *
+     * @param refNum
      */
     public void setRefNum(int refNum) {
         this.refNum = refNum;
     }
-    
+
     /**
      * Get one of the connected roads
+     *
      * @param id Which road to get (1-3)
      * @return The road object. road IDs outside the 1-3 range will return null
      */
@@ -176,21 +188,23 @@ public class NodeSettlement extends WorldObject {
         switch (id) {
             case 1: // Road 1
                 return road1;
-                
+
             case 2: // Road 2
                 return road2;
-                
+
             case 3: // Road 3
                 return road3;
-                
+
             default: // invalid id
                 return null;
         }
     }
-    
+
     /**
      * Set one of the connected roads
-     * @param id Which road to set (1-3) will do nothing if outside the 1-3 range
+     *
+     * @param id Which road to set (1-3) will do nothing if outside the 1-3
+     * range
      * @param road The road to save a reference to
      */
     public void setRoad(int id, NodeRoad road) {
@@ -199,20 +213,21 @@ public class NodeSettlement extends WorldObject {
             case 1: // Road 1
                 this.road1 = road;
                 break;
-                
+
             case 2: // Road 2
                 this.road2 = road;
                 break;
-                
+
             case 3: // Road 3
                 this.road3 = road;
                 break;
         }
         // other values have no effect
     }
-    
+
     /**
      * Get one of the connected world tiles
+     *
      * @param id Which tile to get (1-3)
      * @return The tile object. tile IDs outside the 1-3 range will return null
      */
@@ -221,21 +236,23 @@ public class NodeSettlement extends WorldObject {
         switch (id) {
             case 1: // Tile 1
                 return hex1;
-                
+
             case 2: // Tile 2
                 return hex2;
-                
+
             case 3: // Tile 3
                 return hex3;
-                
+
             default: // invalid id
                 return null;
         }
     }
-    
+
     /**
      * Set one of the connected tiles
-     * @param id Which tile to set (1-3) will do nothing if outside the 1-3 range
+     *
+     * @param id Which tile to set (1-3) will do nothing if outside the 1-3
+     * range
      * @param hex The tile to save a reference to
      */
     public void setTile(int id, Tile hex) {
@@ -244,41 +261,50 @@ public class NodeSettlement extends WorldObject {
             case 1: // Tile 1
                 this.hex1 = hex;
                 break;
-                
+
             case 2: // Tile 2
                 this.hex2 = hex;
                 break;
-                
+
             case 3: // Tile 3
                 this.hex3 = hex;
                 break;
         }
         // other values have no effect
     }
-    
-    
+
     /**
-     * Check which road reference the passed road is stored in (1-3), or 0 if 
-     * this does not contain a reference to the road 
+     * Check which road reference the passed road is stored in (1-3), or 0 if
+     * this does not contain a reference to the road
+     *
      * @param road The road to get the reference id of
-     * @return The reference ID from 1-3, or 0 if the road is not referenced by this settlement
+     * @return The reference ID from 1-3, or 0 if the road is not referenced by
+     * this settlement
      */
     public int getRoadNumber(NodeRoad road) {
         // Return the id of the reference
         // Looking for object references, not attribute equality
-        if      (road == road1) { return 1; } // Matches road 1
-        else if (road == road2) { return 2; } // Matches road 2
-        else if (road == road3) { return 3; } // Matches road 3
-        else                    { return 0; } // Road not referenced
+        if (road == road1) {
+            return 1;
+        } // Matches road 1
+        else if (road == road2) {
+            return 2;
+        } // Matches road 2
+        else if (road == road3) {
+            return 3;
+        } // Matches road 3
+        else {
+            return 0;
+        } // Road not referenced
     }
-    
-    
+
     /**
      * Create an identical settlement node with the same attributes
+     *
      * @return The new settlement node
      */
     @Override
-    public NodeSettlement clone() {    
+    public NodeSettlement clone() {
         // Create a copy of the settlement with the same attributes
         NodeSettlement copy = new NodeSettlement(xPos, yPos, road1, road2, road3, hex1, hex2, hex3, refNum);
         // Set the player id
@@ -287,28 +313,30 @@ public class NodeSettlement extends WorldObject {
         copy.large = large;
         return copy;
     }
-    
+
     /**
      * Compare this road to another to check if they are the same
+     *
      * @param other The road node to compare to
      * @return If the roads are the same or not
      */
     public boolean equals(NodeSettlement other) {
         // Compare all of the properties of the settlement nodes
-        return super.equals(other) 
+        return super.equals(other)
                 && player == other.player
                 && large == other.large
                 && road1 == other.road1
                 && road2 == other.road2
                 && road3 == other.road3;
     }
-    
+
     /**
      * Create a string representations of the road node
+     *
      * @return The road's attributes as a string
      */
     @Override
-    public String toString(){
+    public String toString() {
         // Create a String out of the tile's attributes
         return "Road Node:\n"
                 + super.toString() + "\n"
