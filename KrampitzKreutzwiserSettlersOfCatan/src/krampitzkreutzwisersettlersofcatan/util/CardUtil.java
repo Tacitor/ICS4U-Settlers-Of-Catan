e7582df72b8df63ea7f642d50ab6bb60506042e7
Tacitor <lukasjkrampitz@gmail.com>
2021-07-26 14:31:36 -0400

    create a new class for card utilities

---
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/util/CardUtil.java
@@ -0,0 +1,14 @@
+/*
+ * Lukas Krampitz
+ * Jul 26, 2021
+ * A utility class for Resource and Development Cards
+ */
+package krampitzkreutzwisersettlersofcatan.util;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class CardUtil {
+    
+}
