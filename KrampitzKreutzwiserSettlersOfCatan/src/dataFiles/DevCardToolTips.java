00bab65c2a97bd8b5708718ae04e1f29ea7ecc82
Tacitor <lukasjkrampitz@gmail.com>
2021-09-11 14:16:05 -0400

    spelling for the tool tip text

--- KrampitzKreutzwiserSettlersOfCatan/src/dataFiles/DevCardToolTips.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/dataFiles/DevCardToolTips.java
@@ -13,9 +13,9 @@ public class DevCardToolTips {
 
     //sigle vars
     private final static String KNIGHT_TIP = "Knight Card: Use this card to move the robber to any Tile. Any player with a building on the tile can be stolen from.";
-    private final static String ROAD_TIP = "Road Building Card: Use this card to build two new roads for free. Regular rules still apply, only build cost is ommited.";
-    private final static String MONOPOLY_TIP = "Monopoly Card: Use this card to select one resource type. You will then recieve all the cards of the type from all players.";
-    private final static String YOP_TIP = "Year of Pleanty Card: Use this card to select one resource type. You will then recieve two of the recouse type.";
+    private final static String ROAD_TIP = "Road Building Card: Use this card to build two new roads for free. Regular rules still apply, only build cost is omitted.";
+    private final static String MONOPOLY_TIP = "Monopoly Card: Use this card to select one resource type. You will then receive all the cards of the type from all players.";
+    private final static String YOP_TIP = "Year of Plenty Card: Use this card to select one resource type. You will then receive two of the resource type.";
     private final static String MARKET_TIP = "Market Card: This is a Victory Point card. You built a Market for 1 VP. Others cannot see this point publicly.";
     private final static String UNI_TIP = "University Card: This is a Victory Point card. You built a University for 1 VP. Others cannot see this point publicly.";
     private final static String HALL_TIP = "Great Hall Card: This is a Victory Point card. You built a Great Hall for 1 VP. Others cannot see this point publicly.";
