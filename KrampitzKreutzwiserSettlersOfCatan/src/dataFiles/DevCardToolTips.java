d904c5800dc8a45944ce90c1b4246881729896b0
Tacitor <lukasjkrampitz@gmail.com>
2021-09-11 01:41:02 -0400

    add full text for all the dev cards

--- KrampitzKreutzwiserSettlersOfCatan/src/dataFiles/DevCardToolTips.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/dataFiles/DevCardToolTips.java
@@ -12,15 +12,15 @@ package dataFiles;
 public class DevCardToolTips {
 
     //sigle vars
-    private final static String KNIGHT_TIP = "Knight";
-    private final static String ROAD_TIP = "Road Building: Use this card to build two new roads for free. Regular rules still apply, only build cost is ommited.";
-    private final static String MONOPOLY_TIP = "Monopoly";
-    private final static String YOP_TIP = "Year of Pleanty";
-    private final static String MARKET_TIP = "Market";
-    private final static String UNI_TIP = "University";
-    private final static String HALL_TIP = "Great Hall";
-    private final static String CHALEL_TIP = "Chapel";
-    private final static String LIBRARY_TIP = "Library";
+    private final static String KNIGHT_TIP = "Knight Card: Use this card to move the robber to any Tile. Any player with a building on the tile can be stolen from.";
+    private final static String ROAD_TIP = "Road Building Card: Use this card to build two new roads for free. Regular rules still apply, only build cost is ommited.";
+    private final static String MONOPOLY_TIP = "Monopoly Card: Use this card to select one resource type. You will then recieve all the cards of the type from all players.";
+    private final static String YOP_TIP = "Year of Pleanty Card: Use this card to select one resource type. You will then recieve two of the recouse type.";
+    private final static String MARKET_TIP = "Market Card: This is a Victory Point card. You built a Market for 1 VP. Others cannot see this point publicly.";
+    private final static String UNI_TIP = "University Card: This is a Victory Point card. You built a University for 1 VP. Others cannot see this point publicly.";
+    private final static String HALL_TIP = "Great Hall Card: This is a Victory Point card. You built a Great Hall for 1 VP. Others cannot see this point publicly.";
+    private final static String CHALEL_TIP = "Chapel Card: This is a Victory Point card. You built a Chapel for 1 VP. Others cannot see this point publicly.";
+    private final static String LIBRARY_TIP = "Library Card: This is a Victory Point card. You built a Library for 1 VP. Others cannot see this point publicly.";
     //master array
     private final static String[] TOOLTIPS = new String[]{KNIGHT_TIP, ROAD_TIP, MONOPOLY_TIP, YOP_TIP, MARKET_TIP, UNI_TIP, HALL_TIP, CHALEL_TIP, LIBRARY_TIP};
 
