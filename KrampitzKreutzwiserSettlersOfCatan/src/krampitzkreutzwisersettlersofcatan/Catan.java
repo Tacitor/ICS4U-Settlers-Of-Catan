421d0ae233e40b65a5fa1692b3084bb0310ecabd
Tacitor <lukasjkrampitz@gmail.com>
2021-09-22 13:44:19 -0400

    start work on adding a chimney to the settlements for smoke

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/Catan.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/Catan.java
@@ -18,7 +18,7 @@ public class Catan {
     public static GamePanel gamePanel;
     public static MainMenu menu;
     public static final String SAVE_FILE_VER = "V14"; //the save file version needed    
-    public static final String GAME_VER = "v5.1.3 - Jason v5.0.0 Patch"; //the version of the game/program
+    public static final String GAME_VER = "pre-v5.1.4 - Animation Update"; //the version of the game/program
 
     //fast pulse vars
     private static long prevTime;
--- KrampitzKreutzwiserSettlersOfCatan/src/textures/ImageRef.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/textures/ImageRef.java
@@ -65,7 +65,7 @@ public class ImageRef {
     public final static Image BLUE_HOUSE_L = new ImageIcon(ImageRef.class.getResource("playerPieces/blueHouseL.png")).getImage();
     public final static Image RED_HOUSE_L = new ImageIcon(ImageRef.class.getResource("playerPieces/redHouseL.png")).getImage();
     public final static Image BLUE_HOUSE_S = new ImageIcon(ImageRef.class.getResource("playerPieces/blueHouseS.png")).getImage();
-    public final static Image RED_HOUSE_S = new ImageIcon(ImageRef.class.getResource("playerPieces/redHouseS.png")).getImage();
+    public final static Image RED_HOUSE_S = new ImageIcon(ImageRef.class.getResource("playerPieces/redHouseSmoke.png")).getImage();
     public final static Image BLANK_HOUSE = new ImageIcon(ImageRef.class.getResource("playerPieces/blankHouse.png")).getImage(); // Blank image for unowned settlement nodes 
     public final static Image WHITE_HOUSE_L = new ImageIcon(ImageRef.class.getResource("playerPieces/whiteHouseL.png")).getImage();
     public final static Image ORANGE_HOUSE_L = new ImageIcon(ImageRef.class.getResource("playerPieces/orangeHouseL.png")).getImage();
@@ -83,7 +83,7 @@ public class ImageRef {
     public final static Image WOOD_BACKGROUND = new ImageIcon(ImageRef.class.getResource("util/background.png")).getImage();
 
     //the image for the building materials
-    public final static Image MATERIAL_KEY = new ImageIcon(ImageRef.class.getResource("bigCard/buildKey.png")).getImage();
+    public final static Image MATERIAL_KEY = new ImageIcon(ImageRef.class.getResource("bigCard/buildKeySmoke.png")).getImage();
     
     //current player indecator
     public final static Image PLAYER_RED = new ImageIcon(ImageRef.class.getResource("playerIcons/playerRed.png")).getImage();
