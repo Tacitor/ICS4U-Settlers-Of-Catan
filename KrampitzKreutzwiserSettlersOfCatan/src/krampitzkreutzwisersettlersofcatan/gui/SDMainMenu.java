62b1b1540cd2c7f8397f60a6e9327bb3cdc83101
Tacitor <lukasjkrampitz@gmail.com>
2023-01-01 23:39:27 -0500

    add in the classes for the new SetterDev (SD) based menus.

---
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/gui/SDMainMenu.java
@@ -0,0 +1,14 @@
+/*
+ * Lukas Krampitz
+ * Jan 1, 2023
+ * The JPanel for the Main Menu now using Settler Dev Buttons.
+ */
+package krampitzkreutzwisersettlersofcatan.gui;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class SDMainMenu extends javax.swing.JPanel {
+    
+}
---
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/gui/SDMenuFrame.java
@@ -0,0 +1,14 @@
+/*
+ * Lukas Krampitz
+ * Jan 1, 2023
+ * The JFrame for the menu elements of the game. Will hold the other SD Panels with settler buttons
+ */
+package krampitzkreutzwisersettlersofcatan.gui;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class SDMenuFrame extends javax.swing.JFrame {
+
+}
