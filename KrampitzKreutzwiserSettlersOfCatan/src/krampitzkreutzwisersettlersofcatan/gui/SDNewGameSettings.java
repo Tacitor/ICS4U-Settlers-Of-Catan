d14d5b1951bc1cf632bd290ea10566a7bee0de27
Tacitor <lukasjkrampitz@gmail.com>
2023-08-26 19:05:34 -0400

    add in a new class for the settings for a new game. Right now is a bare bones copy from the credits panel

---
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/gui/SDNewGameSettings.java
@@ -0,0 +1,261 @@
+/*
+ * Lukas Krampitz
+ * August 26, 2023
+ * The JPanel for the New Game Settings now using Settler Dev Buttons.
+ */
+package krampitzkreutzwisersettlersofcatan.gui;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import krampitzkreutzwisersettlersofcatan.worldObjects.buttons.SettlerBtn;
+import krampitzkreutzwisersettlersofcatan.worldObjects.buttons.SettlerLbl;
+import textures.ImageRef;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class SDNewGameSettings extends javax.swing.JPanel implements MouseMotionListener {
+
+    private SDMenuFrame sDMenuFrame;
+    private static double localScaleFactor; //The factor to scale this panel by when drawing elemets
+    private int mouseMotionPosX; //acording to the MouseMotionListener where is the mouse located
+    private int mouseMotionPosY;
+
+    //Settler Compoments
+    private SettlerBtn exitBtn;
+    //The array for the buttons
+    private SettlerBtn[] settlerBtns;
+    //Settler Labels
+    private SettlerLbl newGameSettingsLbl;
+    //The array for the buttons
+    private SettlerLbl[] settlerLbls;
+
+    //Fonts
+    public Font COMPASS_GOLD;
+
+    /**
+     * Main Constructor
+     *
+     * @param sDFrame
+     */
+    public SDNewGameSettings(SDMenuFrame sDFrame) {
+        sDMenuFrame = sDFrame;
+
+        COMPASS_GOLD = sDMenuFrame.setUpCompassGoldFont();
+
+        //add the mouse motion listener
+        addMouseMotionListener(this);
+
+        //add a mouse listener that call the mouse click event handler
+        addMouseListener(new MouseAdapter() {
+            /**
+             * Triggered when the user clicks on the SDCreditsPanel. Calls the
+             * menu panel's click event method.
+             *
+             * @param event
+             */
+            @Override
+            public final void mouseReleased(MouseEvent event) {
+                //send the mouse event to the menu panel click handler
+                mouseClick(event);
+            }
+        });
+
+        //setup the buttons
+        exitBtn = new SettlerBtn(true, 0, 23);
+        //add them to the array
+        settlerBtns = new SettlerBtn[]{exitBtn};
+        //Setup the labels
+        newGameSettingsLbl = new SettlerLbl("Le New Game Settings");
+        newGameSettingsLbl.setForeground(DomesticTradePanel.BEIGE_COLOUR);
+        newGameSettingsLbl.setUseNewLineChar(false);
+        //add them to the array
+        settlerLbls = new SettlerLbl[]{newGameSettingsLbl};
+
+    }
+
+    /**
+     * Override the JPanel default paintComponent() method.
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+
+        //call the paintCompnent from the super class
+        super.paintComponent(g);
+        //call the custom layer
+        draw(g);
+    }
+
+    /**
+     * Draw the UI for the SDPanel
+     *
+     * @param g
+     */
+    private void draw(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        //update the scale factor
+        localScaleFactor = sDMenuFrame.calcScaleFactor(this);
+
+        //update the button positions
+        settlerVarPos(g2d);
+
+        //draw the background image
+        g2d.drawImage(ImageRef.WOOD_BACKGROUND,
+                0,
+                0,
+                this.getWidth(),
+                this.getHeight(), this);
+
+        g2d.setFont(new Font(COMPASS_GOLD.getName(), Font.PLAIN, localScaleInt(120)));
+        g2d.setColor(DomesticTradePanel.BEIGE_COLOUR);
+
+        //Draw the Title
+        g2d.drawString("Header Text",
+                (this.getWidth() / 2) - (g2d.getFontMetrics().stringWidth("Header Text") / 2),
+                localScaleInt(100));
+
+        g2d.setFont(new Font(COMPASS_GOLD.getName(), Font.PLAIN, localScaleInt(70)));
+
+        //=-=-=-=-=-=-=-=-=-= Draw the Settlerbuttons =-=-=-=-=-=-=-=-=-=
+        for (SettlerBtn btn : settlerBtns) {
+            btn.updateButtonImages();
+            btn.updateText();
+
+            //draw the base        
+            sDMenuFrame.drawSettlerBtn(g2d, btn.getBaseImage(), btn, 0, this);
+
+            //draw the text
+            sDMenuFrame.drawSettlerBtn(g2d, btn.getTextImage(), btn, 0, this);
+
+            //draw the disabled overlay if required
+            if (!btn.isEnabled()) {
+                sDMenuFrame.drawSettlerBtn(g2d, btn.getDisabledImage(), btn, 0, this);
+            }
+            //draw the mouseHover overlay if required
+            if (btn.isMouseHover()) {
+                sDMenuFrame.drawSettlerBtn(g2d, btn.getHoverImage(), btn, 1, this);
+            }
+
+        }
+
+        //=-=-=-=-=-=-=-=-=-= END OF the drawing of Settlerbuttons =-=-=-=-=-=-=-=-=-=
+        //go through and draw all the labels
+        for (SettlerLbl settlerLbl : settlerLbls) {
+            settlerLbl.draw(g2d, localScaleFactor);
+        }
+    }
+
+    /**
+     * Update the positions of the SD Components
+     */
+    private void settlerVarPos(Graphics2D g2d) {
+        //Label Loop
+        newGameSettingsLbl.setFont(new Font(COMPASS_GOLD.getName(), Font.PLAIN, localScaleInt(70)));
+        //calc the number of lines for the labels that will be multi line
+        for (SettlerLbl lbl : settlerLbls) {
+            if (lbl.getUseNewLineChar()) {
+                lbl.calcNumLinesCarriageReturn();
+            }
+        }
+
+        newGameSettingsLbl.setXPos(localScaleInt(50));
+        newGameSettingsLbl.setYPos(localScaleInt(300));
+
+        exitBtn.setXPos(this.getWidth() / 2 - sDMenuFrame.getImgWidthLocal(exitBtn.getBaseImage(), this) / 2);
+        //Line this up with the exit button from the SDMainMenuPanel.java
+        exitBtn.setYPos(localScaleInt(250) + ((localScaleInt(SDMenuFrame.MENU_PACKING_HEIGHT) + sDMenuFrame.getImgHeightLocal(exitBtn.getBaseImage(), this)) * 6));
+
+    }
+
+    /**
+     * Handles click releases from mouse input.
+     *
+     * @param evt
+     */
+    public void mouseClick(MouseEvent evt) {
+
+        //check if the player clicked on one of the SettlerBtns
+        //loop through all the custom buttons
+        for (SettlerBtn btn : settlerBtns) {
+            if (evt.getX() > btn.getXPos()
+                    && evt.getY() > btn.getYPos()
+                    && evt.getX() < (btn.getXPos() + sDMenuFrame.getImgWidthLocal(btn.getBaseImage(), this))
+                    && evt.getY() < (btn.getYPos() + sDMenuFrame.getImgHeightLocal(btn.getBaseImage(), this))
+                    && btn.isEnabled()) { //and that it is enabled
+
+                //check the button that was pressed
+                if (btn.equals(exitBtn)) { //if it was the exit game button
+                    exitBtnActionPerformed();
+                }
+            }
+        }
+    }
+
+    /**
+     * Update the positions of the Mouse Pointer
+     *
+     * @param e
+     */
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        //System.out.println("Moved: " + e.getX() + ", an " + e.getY());
+        mouseMotionPosX = e.getX();
+        mouseMotionPosY = e.getY();
+        mouseMoveAction();
+    }
+
+    /**
+     * Handles mouse pointer movement.
+     */
+    private void mouseMoveAction() {
+
+        //check if the player moved the mouse over one of the SettlerBtns
+        //loop through all the custom buttons
+        for (SettlerBtn btn : settlerBtns) {
+            if (mouseMotionPosX > btn.getXPos()
+                    && mouseMotionPosY > btn.getYPos()
+                    && mouseMotionPosX < (btn.getXPos() + sDMenuFrame.getImgWidthLocal(btn.getBaseImage(), this))
+                    && mouseMotionPosY < (btn.getYPos() + sDMenuFrame.getImgHeightLocal(btn.getBaseImage(), this))
+                    && btn.isEnabled()) { //and that it is enabled
+
+                //set the hover
+                btn.setmouseHover(true);
+
+            } else {
+
+                //make suer there is no hover over that button
+                btn.setmouseHover(false);
+            }
+
+        }
+
+        repaint();
+    }
+
+    /**
+     * Scale a number to match the resolution of the screen
+     *
+     * @param num
+     * @return
+     */
+    public static int localScaleInt(int num) {
+        return (int) (num / localScaleFactor);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        //System.out.println("Mouse Dragged");
+    }
+
+    private void exitBtnActionPerformed() {
+        exitBtn.setmouseHover(false);
+        sDMenuFrame.switchPanel(this, sDMenuFrame.getSDMainMenuPanel());
+    }
+}
