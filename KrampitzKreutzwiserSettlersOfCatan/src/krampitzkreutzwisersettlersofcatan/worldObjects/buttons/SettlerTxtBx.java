730129ff62e9399eb81860927a6062e39b98df61
Tacitor <lukasjkrampitz@gmail.com>
2023-09-05 13:33:57 -0400

    add in the constructors to the SD text-box

--- KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/worldObjects/buttons/SettlerTxtBx.java
+++ KrampitzKreutzwiserSettlersOfCatan/src/krampitzkreutzwisersettlersofcatan/worldObjects/buttons/SettlerTxtBx.java
@@ -18,6 +18,43 @@ public class SettlerTxtBx extends SettlerComponent {
     protected int startDisplayPos; //the position of where to start displaying the text withing the box.
     protected char[] chars; //the char array that will contain the text of the text box.
 
+    /**
+     * Primary Constructor
+     *
+     * @param enabled
+     * @param type
+     */
+    public SettlerTxtBx(boolean enabled, int type) {
+        xPos = 0;
+        yPos = 0;
+
+        this.enabled = enabled;
+        this.type = type;
+        mouseHover = false;
+        tabSelected = false;
+
+        updateText();
+        updateButtonImages();
+    }
+
+    /**
+     * Secondary Constructor
+     *
+     * @param xPos
+     * @param yPos
+     * @param enabled
+     * @param type
+     */
+    public SettlerTxtBx(int xPos, int yPos, boolean enabled, int type) {
+        this(enabled, type);
+
+        this.xPos = xPos;
+        this.yPos = yPos;
+
+        updateText();
+        updateButtonImages();
+    }
+
     @Override
     public void updateButtonImages() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
