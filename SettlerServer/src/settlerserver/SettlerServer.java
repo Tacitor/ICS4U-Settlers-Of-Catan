20c820b18e37fcd25be87394f3de2632a903f69b
Tacitor <lukasjkrampitz@gmail.com>
2024-07-30 22:16:31 -0400

    Add a new project for the server software

---
+++ SettlerServer/src/settlerserver/SettlerServer.java
@@ -0,0 +1,22 @@
+/*
+ * Lukas Krampitz
+ * Jul 30, 2024
+ * A secondary project for the dedicated serverside software to run the catan server
+ */
+package settlerserver;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class SettlerServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Test");
+    }
+    
+}
