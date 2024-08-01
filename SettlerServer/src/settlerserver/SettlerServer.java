97ec3d148b62a3fbff03199f013d1fa5834e5407
Tacitor <lukasjkrampitz@gmail.com>
2024-07-31 21:33:21 -0400

    test simple hard coded server

--- SettlerServer/src/settlerserver/SettlerServer.java
+++ SettlerServer/src/settlerserver/SettlerServer.java
@@ -16,7 +16,26 @@ public class SettlerServer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Test");
+        System.out.println("Settting up server for 2 players");
+        serverStartUp(2, 25570);
+
     }
-    
+
+    /**
+     * Create the local server using the old @Depricated class. This going
+     * forward only the server in the new SettlerServer project and package is
+     * maintained.
+     */
+    private static void serverStartUp(int numPlayers, int port) {
+        CatanServer leServer = new CatanServer(numPlayers, port);
+
+        //create a new thread for the server
+        Thread t = new Thread(() -> {
+            leServer.acceptConnections();
+        });
+
+        //start running the server
+        t.start();
+    }
+
 }
